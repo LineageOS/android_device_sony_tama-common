7053b4ea8263386fefe365b44cbb56ea8bf233b4
LuK1337 <priv.luk@gmail.com>
2019-12-08 23:08:57 +0100

    tama: Automatically detect msim variants
    Change-Id: I3c81c6405bbca6c79bc50544e6cf7c01ac2fb84d

--- common.mk
+++ common.mk
@@ -167,6 +167,7 @@ PRODUCT_PACKAGES += \
     init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.factory.rc \
+    init.qcom.msim.sh \
     init.qcom.post_boot.sh \
     init.qcom.power.rc \
     init.qcom.rc \
