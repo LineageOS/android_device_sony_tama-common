a64092178cd68c1b2dad08ed9d103c71ef3e8343
Olivier Karasangabo <olivier@lineageos.org>
2020-04-30 14:12:10 +0200

    tama: Welcome xz2c to the family
    Change-Id: I2fa8f83737d4e0fe1a2c17b60c37b6f231a49d22

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter akari apollo akatsuki,$(TARGET_DEVICE)),)
+ifneq ($(filter akari xz2c akatsuki,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
