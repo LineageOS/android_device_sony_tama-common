b408bcf46ecbae1da3aac7aaa215bd596ef8cf44
Olivier Karasangabo <olivier@lineageos.org>
2020-07-21 23:36:30 +0200

    tama: Build vendor.qti.hardware.btconfigstore@1.0.so from source
    * Also remove vendor.qti.hardware.btconfigstore@1.0-impl libs while at it
    
    Change-Id: I2763d6f4e55e60b258906ef90c03a5f2edede5ee

--- common.mk
+++ common.mk
@@ -144,7 +144,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Boot control
 PRODUCT_PACKAGES += \
