ed33f95808e8a15298dc1a3da6fe4668b7538550
Harshal Ahire <hahire@codeaurora.org>
2020-08-05 02:48:23 +0530

    tama-common: enable sound trigger hidl hal 2.3
    Change-Id: Ieac067b937c06f1ce4f74abf0b884fc1bf92b5c9

--- common.mk
+++ common.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.r_submix.default \
     audio.usb.default \
     libaudioroute.vendor \
