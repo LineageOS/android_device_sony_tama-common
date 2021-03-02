c0ce50cac4c896f6155c54e99b75a974abbad7e5
Andrea <linckandrea01@gmail.com>
2021-03-03 00:53:31 +0100

    tama: imports hardware/google/pixel
    Change-Id: I8e4adb172ee1fc7ded8609de5b496073199da920

--- common.mk
+++ common.mk
@@ -343,7 +343,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
