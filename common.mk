cc9ab1dbb97e442faf64920177a25bac01cd2ad9
Colin S <bugfactory+los@inqui.site>
2022-03-25 20:01:44 -0500

    tama-common: adapt ganges-ims for tama
    * The modem_switcher service requires
      `ro.actionable_compatible_property.enabled=false` due to the use
      of non-standard property names as init triggers.
    
    * Includes sepolicy fixes:
    
      * permit modem-switcher to set `persist.somc.cust.`
      * permit HAL access
    
    Change-Id: Id0af85a41c56cadf954ef95fac8349b5057d276d

--- common.mk
+++ common.mk
@@ -277,13 +277,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Modem switcher
-ifneq ($(filter %_kirin %_mermaid,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += \
     ModemConfig
 
 PRODUCT_COPY_FILES += \
     $(shell find $(LOCAL_PATH)/modem-config -type f -printf '%p:$(TARGET_COPY_OUT_VENDOR)/modemconf/%P\n')
-endif
 
 # Net
 PRODUCT_PACKAGES += \
