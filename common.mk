3dbe7fa722cd25a37b994ff25051d890ea3e12aa
Danny Trunk <dtrunk90@gmail.com>
2022-09-18 20:12:25 +0200

    tama-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- common.mk
+++ common.mk
@@ -208,7 +208,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor
 
 # Fingerprint
