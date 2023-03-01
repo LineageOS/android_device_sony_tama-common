980c568fb0b3afa16b2f36ba6de97a41ac45dd94
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:53:44 +0800

    tama-common: health: Change to fit tama's stock nodes
    Change-Id: I356db9ecc2d9004a1181345fb2ae21c40c4022bf

--- aidl/health/ChargingControl.cpp
+++ aidl/health/ChargingControl.cpp
@@ -19,8 +19,7 @@ namespace lineage {
 namespace health {
 
 static const std::vector<ChargingEnabledNode> kChargingEnabledNodes = {
-    {"/sys/class/power_supply/battery/input_suspend", "0", "1"},
-    {"/sys/class/power_supply/battery/charging_enabled", "1", "0"},
+    {"/sys/class/power_supply/battery/smart_charging_interruption", "0", "1"},
 };
 
 static bool fileExists(const std::string& path) {
