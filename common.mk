49d75c05c49c491519ff8c3bef9b8b9ddb7ecc1b
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    tama-common: Build lineage health HAL and related sepolicies
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad

--- common.mk
+++ common.mk
@@ -259,6 +259,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
