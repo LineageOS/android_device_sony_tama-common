34c4b4589a24d1300f59131c339a9e65cdde5e99
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    tama-common: Build lineage health HAL and related sepolicies
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad

--- common.mk
+++ common.mk
@@ -185,7 +185,8 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     qns.fstab \
     sensor_json_transfer.sh \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    ueventd.tama.rc
 
 # Display
 PRODUCT_PACKAGES += \
@@ -258,6 +259,10 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
+
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.tama
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
