b0f7ee2eb2d640eb25385e54facec91a391921b3
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    tama-common: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- common.mk
+++ common.mk
@@ -325,8 +325,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
