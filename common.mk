0862323322e5ccfbbc0be60412df8edd72a17e8e
Bruno Martins <bgcngm@gmail.com>
2024-03-21 16:31:24 +0000

    tama-common: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- common.mk
+++ common.mk
@@ -402,10 +402,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
