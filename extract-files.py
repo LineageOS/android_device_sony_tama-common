ce88328ad122999c17f151322f0f1f9308344c61
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 21:46:46 +0300

    tama-common: Patch libwfdservice to resolve duplicate dependencies
    Fixes:
    error: module "libwfdservice" variant
    "android_arm_armv8-a_cortex-a76_static": depends on multiple
    versions of the same aidl_interface:
    android.media.audio.common.types-V2-cpp-source,
    android.media.audio.common.types-V3-cpp-source
    
    Change-Id: I8086a80fcc238d6b03d16640ee2c4e134129bbab

--- extract-files.py
+++ extract-files.py
@@ -68,6 +68,8 @@ blob_fixups: blob_fixups_user_type = {
         'system_ext/etc/permissions/telephonyservice.xml'
     ): blob_fixup()
         .regex_replace('/product/framework/', '/system_ext/framework/'),
+    'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
     ('system_ext/lib/lib-imsvideocodec.so', 'system_ext/lib64/lib-imsvideocodec.so'): blob_fixup()
         .add_needed('libgui_shim.so'),
     'vendor/bin/pm-service': blob_fixup()
