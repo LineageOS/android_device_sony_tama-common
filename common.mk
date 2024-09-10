adc155a7ae304da6c6d6ebf3ff6ac4b0fbc7a05a
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    tama-common: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- common.mk
+++ common.mk
@@ -214,7 +214,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim
 
 # Fingerprint
 PRODUCT_PACKAGES += \
