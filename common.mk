3074723c52d044f0a2ba88a9ff2a5b5f922c4939
Marc Bourgoin <themard85@gmail.com>
2024-11-21 13:24:16 -0700

    tama-common: Remove copy libutils-v33
    With enforced elf checks coming on A16, let's switch to the provided one from compat
    
    Change-Id: I193682ca769e902b7a8b2edd850ea14297a71dec

--- common.mk
+++ common.mk
@@ -426,8 +426,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
+PRODUCT_PACKAGES += \
+    libutils-v33
 
 # WiFi
 PRODUCT_PACKAGES += \
