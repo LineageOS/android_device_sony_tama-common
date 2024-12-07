d19f31689d1b5fe1ebbcd58146dba1bafcef1b57
Nick Reuter <nreuter85@gmail.com>
2024-12-07 15:59:17 -0600

    tama-common: Bump libwfdservice to android.media.audio.common.types-V4
    Change-Id: I81060d8c73430a9598c676e18dc04e31a2f31c0c

--- extract-files.py
+++ extract-files.py
@@ -69,7 +69,7 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .regex_replace('/product/framework/', '/system_ext/framework/'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     ('system_ext/lib/lib-imsvideocodec.so', 'system_ext/lib64/lib-imsvideocodec.so'): blob_fixup()
         .add_needed('libgui_shim.so'),
     'vendor/bin/pm-service': blob_fixup()
