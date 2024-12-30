ac07fce56eb48096339fe4e3d25822d5c985bb59
linckandrea <linckandrea01@gmail.com>
2024-12-30 16:59:55 +0100

    tama-common: introduce touchHAL to manage glove mode
      * kanged from sake as @3a8f1b3d8f07e9654d7c9633779d1f70a5a399a9
      * gesture & high polling rate support were removed
      * use tama common sys path for touchscreen features
    
    Change-Id: Iff6d8ace406b9f466b272d3632b23b97d6a7fffe

--- common.mk
+++ common.mk
@@ -411,6 +411,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
+# Touch
+PRODUCT_PACKAGES += \
+    lineage.touch@1.0-service.tama
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
